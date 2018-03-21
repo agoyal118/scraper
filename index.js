da1620798c4fd78ed487088eaed6477dcdeceb43
kyrie irving <kyrie@kyries-MacBook-Pro.local>
2018-03-21 16:51:31 -0500

    getting correct links

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ var url = 'https://webpoint.usaweightlifting.org/wp15/Events2/Events.wp?evt_Cate
 request(url, function(err, response, html) {
 	if(!err) {
 		var $ = cheerio.load(html);
-		var allItems = $('div.ilb.vt.push-60.pad-5').children();
-		//console.log(allItems);
+		var allItems = $('div.ilb.vt.push-60.pad-5');
 		var items = [];
 		allItems.each(function(index) {
-			//console.log("1");
-			items.push($('div.ilb.vt.push-60.pad-5').children().eq(index).find('h3 > a').attr("href"));
+			items.push($('div.ilb.vt.push-60.pad-5').eq(index).find('a').attr('href'));
 		});
 		console.log(items);
 	}
