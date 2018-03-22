6edb03d34ab04957d0f8113c953715879c2c6445
kyrie irving <kyrie@kyries-MacBook-Pro.local>
2018-03-21 19:45:37 -0500

    puts all names and emails in name_and_email array as json objects

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var jsonexport = require('jsonexport');
 
 var baseURL = 'https://webpoint.usaweightlifting.org';
 var homeURL = 'https://webpoint.usaweightlifting.org/wp15/Events2/Events.wp?evt_CategoryID=12';
-var urlList = [];
 var name_and_email = [];
-var testURL = 'https://webpoint.usaweightlifting.org/wp15/Events2/ViewEvt.wp?EventID=137709';
 
 request(homeURL, function(err, response, html) {
 	if(!err) {
 		var $ = cheerio.load(html);
 		var allItems = $('div.ilb.vt.push-60.pad-5');
 		allItems.each(function(index) {
-			urlList.push(baseURL + $('div.ilb.vt.push-60.pad-5').eq(index).find('a').attr('href'));
+			var navigateToNewURL = baseURL + $('div.ilb.vt.push-60.pad-5').eq(index).find('a').attr('href');
+			request(navigateToNewURL, function(err, response, html) {
+				if (!err) {
+					var $ = cheerio.load(html);
+					var tempName = $("td.mfff:contains('Coordinator:')").siblings().text();
+					var tempEmail = $("td.mfff:contains('Email:')").siblings().text();
+					name_and_email.push({
+										name: tempName,
+										email: tempEmail
+					});
+				}
+			});
 		});
-		console.log(urlList);
-	}
-});
-
-
-request(testURL, function(err, response, html) {
-	if (!err) {
-		var $ = cheerio.load(html);
-		var name = $("td.mfff:contains('Coordinator:')").siblings().text();
-		var email = $("td.mfff:contains('Email:')").siblings().text();
-		console.log(name, email);
 	}
 });
